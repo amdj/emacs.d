0a0ef59512ceb3fc006ee1661caf99e30a3aec97
Anne_ctwtm <j.a.dejong@utwente.nl>
2015-05-22 09:52:10 +0200

    Included elpa

---
+++ elpa/ac-c-headers-20150329.710/ac-c-headers-autoloads.el
@@ -0,0 +1,16 @@
+;;; ac-c-headers-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("ac-c-headers.el") (21801 5215 911818
+;;;;;;  948000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ac-c-headers-autoloads.el ends here
---
+++ elpa/auto-complete-clang-20140409.52/auto-complete-clang-autoloads.el
@@ -0,0 +1,16 @@
+;;; auto-complete-clang-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("auto-complete-clang.el") (21801 5083
+;;;;;;  373357 804000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; auto-complete-clang-autoloads.el ends here
